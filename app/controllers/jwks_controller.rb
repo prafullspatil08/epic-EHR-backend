9dc1dddf641b6fe68d8a4bd365ad51dadc654242
prafullspatil08 <prafull.patil@mindbowser.com>
2025-12-29 22:56:09 +0530

    feat: support JWK loading from environment variable with file fallback

--- app/controllers/jwks_controller.rb
+++ app/controllers/jwks_controller.rb
@@ -1,8 +1,13 @@
 class JwksController < ApplicationController
 
   def index
-    jwk_path = Rails.root.join("/etc/secrets/epic-public.jwk")
-    jwk = JSON.parse(File.read(jwk_path))
-    render json: { keys: [jwk] }
+    jwk_content = ENV['EPIC_PUBLIC_JWK_JSON']
+if jwk_content.blank?
+  # This part runs only if the environment variable is NOT set
+  jwk_path = Rails.root.join('config', 'keys', 'epic-public.jwk')
+  jwk_content = File.read(jwk_path)
+end
+jwk = JSON.parse(jwk_content)
+render json: { keys: [jwk] }
   end
 end
